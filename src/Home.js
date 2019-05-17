22f97d092e7f8649b61e755e92148b6276f6a8af
Adam Grider <adam.h.grider@gmail.com>
2019-05-17 15:54:05 -0400

    add homepage

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+	constructor(props) {
+		super(props);
+		console.log(this.props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Home</h1>
+			</div>
+		);
+	}
+}
+
+export default Home;
